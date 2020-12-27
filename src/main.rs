3a4df2629c58a39cbafbbf5a556d2933a8ef430c
Arnau Reig <arreme@hotmail.es>
2020-12-27 01:52:03 +0100

    The print works now!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let _input = read_all::<u16>("src/input.txt");
+    let _final_input = _input.unwrap();
+    for x in _final_input {
+        println!("{}",x);
+    }
+    //sort
+    //add
 }
+
+fn read_all<T: FromStr>(file_name: &str) -> Result<Vec<T>, <T as FromStr>::Err> {
+    std::fs::read_to_string(file_name)
+        .expect("file not found!")
+        .lines()
+        .map(|x| x.parse())
+        .collect()
+}
+
+
+
+
+
+
+
