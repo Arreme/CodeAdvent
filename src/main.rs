b99729226af92479206239343d7c7551ff3c1d24
Arnau Reig <arreme@hotmail.es>
2021-01-10 23:18:31 +0100

    Sorted

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@ use std::str::FromStr;
 
 fn main() {
     let _input = read_all::<u16>("src/input.txt");
-    let _final_input = _input.unwrap();
+    let mut _final_input = _input.unwrap();
+    sort(&mut _final_input);
     for x in _final_input {
-        println!("{}",x);
+        println!("{}", x);
     }
-    //sort
     //add
+}
+
+fn sort(array: &mut Vec<u16>) {
+    for i in 0..array.len() {
+        for j in 0..array.len() - i - 1 {
+            if array[j + 1] < array[j] {
+                array.swap(j, j + 1);
+            }
+        }
+    }
 }
 
 fn read_all<T: FromStr>(file_name: &str) -> Result<Vec<T>, <T as FromStr>::Err> {
@@ -17,10 +27,3 @@ fn read_all<T: FromStr>(file_name: &str) -> Result<Vec<T>, <T as FromStr>::Err> 
         .map(|x| x.parse())
         .collect()
 }
-
-
-
-
-
-
-
