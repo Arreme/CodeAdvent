643dacc57aa347e7ff1bfeadd85b9022a019fd56
Arnau Reig <arreme@hotmail.es>
2021-01-12 16:30:48 +0100

    CodeAdvent Finished!

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use std::str::FromStr;
 
 fn main() {
-    let _input = read_all::<u16>("src/input.txt");
+    let _input = read_all::<u32>("src/input.txt");
     let mut _final_input = _input.unwrap();
     sort(&mut _final_input);
-    for x in _final_input {
-        println!("{}", x);
+    for x in &_final_input {
+        for y in &_final_input {
+            for z in &_final_input {
+                    match x+y+z {
+                    2020 => println!("Found one! {}",x*y*z),
+                    x if x > 2020 => {
+                        break;
+                    }
+                    _ => {},
+                }
+            }
+        }
     }
-    //add
 }
 
-fn sort(array: &mut Vec<u16>) {
+fn sort(array: &mut Vec<u32>) {
     for i in 0..array.len() {
         for j in 0..array.len() - i - 1 {
             if array[j + 1] < array[j] {
